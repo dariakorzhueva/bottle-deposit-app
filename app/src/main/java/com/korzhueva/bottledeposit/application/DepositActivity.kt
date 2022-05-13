bfe5f65785926e2fd259e921c2537bfc8311fcd2
dkorzhueva <dkorzhueva@dejavoosystems.ru>
2022-05-14 00:30:02 +0700

    Fix deposit activity imports

--- app/src/main/java/com/korzhueva/bottledeposit/application/DepositActivity.kt
+++ app/src/main/java/com/korzhueva/bottledeposit/application/DepositActivity.kt
@@ -2,7 +2,7 @@ package com.korzhueva.bottledeposit.application
 
 import android.app.Activity
 import android.os.Bundle
-import com.korzhueva.bottle.deposi.bottle.deposit.bottledeposit.R
+import com.korzhueva.bottledeposit.R
 
 class DepositActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
