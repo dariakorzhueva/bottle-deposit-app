9571ba308d0ae4bee94b4c6e595cc62ba39fcd74
dkorzhueva <dkorzhueva@dejavoosystems.ru>
2022-06-06 00:41:08 +0700

    Add bottom panel for deposit

---
+++ app/src/main/java/com/korzhueva/bottledeposit/application/core/ui/QuantityBottomPanelView.kt
@@ -0,0 +1,35 @@
+package com.korzhueva.bottledeposit.application.core.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageButton
+import com.korzhueva.bottledeposit.R
+
+class QuantityBottomPanelView : LinearLayout {
+    private lateinit var btnDown: AppCompatImageButton
+    private lateinit var btnUp: AppCompatImageButton
+
+    constructor(context: Context?) : super(context) {
+        setUI()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        setUI()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        setUI()
+    }
+
+    private fun setUI() {
+        val view = inflate(context, R.layout.deposit_bottom_panel, this)
+        btnDown = view.findViewById(R.id.btnDown)
+        btnUp = view.findViewById(R.id.btnUp)
+    }
+}
