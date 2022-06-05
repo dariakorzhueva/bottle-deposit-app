db84fe7af0a235d0b65839e6205a76ab147064d1
dkorzhueva <dkorzhueva@dejavoosystems.ru>
2022-06-06 00:54:30 +0700

    Add dependence for dagger2

--- app/src/main/java/com/korzhueva/bottledeposit/application/DepositApplication.kt
+++ app/src/main/java/com/korzhueva/bottledeposit/application/DepositApplication.kt
@@ -1,6 +1,18 @@
 package com.korzhueva.bottledeposit.application
 
 import android.app.Application
+import com.korzhueva.bottledeposit.application.core.di.ApplicationComponent
+import com.korzhueva.bottledeposit.application.core.di.DaggerApplicationComponent
 
 class DepositApplication : Application() {
+    private lateinit var appComponent: ApplicationComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        val appComponent = DaggerApplicationComponent.create()
+    }
+
+    fun getAppComponent(): ApplicationComponent {
+        return appComponent
+    }
 }
---
+++ app/src/main/java/com/korzhueva/bottledeposit/application/core/di/ApplicationComponent.kt
@@ -0,0 +1,8 @@
+package com.korzhueva.bottledeposit.application.core.di
+
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component
+interface ApplicationComponent
